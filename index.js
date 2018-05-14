cd1ccdc76dce10954e50f035dbceeda492da02d5
Unknown <samuel30810@gmail.com>
2018-05-14 21:11:01 +0800

    add crawler

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('hello');
+const Xray = require('x-ray');
+const x = Xray();
+x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html', 'table > tr', [
+    {
+        No: 'td:nth-child(2)',
+        coursename: 'td:nth-child(4)',
+        teacher: 'td:nth-child(5)',
+    }
+]).write('./course2.json');
